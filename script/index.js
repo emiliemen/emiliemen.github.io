c0846bb2bfdfa7634116d0c5435084589ee5d246
johannzs <johann.zitouni@gmail.com>
2021-02-27 11:26:06 +0100

    Init

---
+++ script/index.js
@@ -0,0 +1,12 @@
+ $('#fullpage').fullpage(
+{
+  sectionsColor: ['#F4F4E', '#F4F4E', '#F4F4E','#F4F4E', '#F4F4E'],
+  sectionSelector: '.vertical-scrolling',
+  navigation: true,
+  navigationPosition: 'left',
+  slidesNavigation: true,
+  controlArrows: false,
+  anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection'],
+  menu: '#menu',
+ 
+});
---
+++ script/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.images').slick({
+        dots: true,
+        adaptiveHeight: true
+    });
+  });
+
+
